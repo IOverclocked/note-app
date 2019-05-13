01a716f06442d74e1be57992d7a678e34ddba95a
IOverclocked <osiatycki.m@gmail.com>
2019-05-13 21:39:57 +0200

    Create GlobalStyle
    Create and set GlobalStyle use createGlobalstyle from styled components

--- src/view/Root/Root.js
+++ src/view/Root/Root.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Button from 'components/Button/Button';
+import GlobalStyle from 'theme/GlobalStyle';
 
 const Root = () => (
   <>
+    <GlobalStyle />
     <h1>Hello note</h1>
     <Button width="500px">Close / Save</Button>
     <Button secondary>Remove</Button>
