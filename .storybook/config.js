a0879de252a37853e603e5c8a08b00ef6faa11a3
IOverclocked <osiatycki.m@gmail.com>
2019-05-16 20:28:28 +0200

    Create components (atoms)
    Create heading with stories and paragraph
    with stories and configurate Provaider theme
    in config.js files in .storiesbook folder

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,14 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../src/theme/mainTheme';
 
 const req = require.context('../src/components', true, /\.stories\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addDecorator(story => <ThemeProvider theme={theme}>{story()}</ThemeProvider>);
 
 configure(loadStories, module);
