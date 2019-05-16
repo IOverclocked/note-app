31ac72bd40be6cc5310e5eec15830bd7df8e923c
IOverclocked <osiatycki.m@gmail.com>
2019-05-16 21:01:48 +0200

    Add styles for input
    Create styles for search input

--- src/components/atoms/Input/Input.stories.js
+++ src/components/atoms/Input/Input.stories.js
@@ -2,4 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Input from './Input';
 
-storiesOf('Input', module).add('Normal', () => <Input placeholder="note" />);
+storiesOf('Input', module)
+  .add('Normal', () => <Input placeholder="Note" />)
+  .add('Search', () => <Input placeholder="Search..." search />);
