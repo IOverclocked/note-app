b5524659aeb16fff93ec6cca6a6991371739a25e
IOverclocked <osiatycki.m@gmail.com>
2019-05-21 19:01:15 +0200

    Styled PageTemplate

--- src/templates/PageTemplate/PageTemplate.js
+++ src/templates/PageTemplate/PageTemplate.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Sidebar from 'components/organisms/Sidebar/Sidebar';
+import Paragraph from 'components/atoms/Paragraph/Paragraph';
+import Input from 'components/atoms/Input/Input';
+import Heading from 'components/atoms/Heading/Heading';
+
+const StyledWrapper = styled.div`
+  padding: 50px 75px;
+`;
+
+const StyledGridWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 40px;
+  padding: 25px 0 0 0;
+`;
+
+const SearchWrapper = styled.div``;
+
+const StyledHeading = styled(Heading)`
+  font-size: ${({ theme }) => theme.fontSize.xl};
+  font-weight: ${({ theme }) => theme.bold};
+`;
+
+const StyledParagraph = styled(Paragraph)`
+  font-size: ${({ theme }) => theme.fontSize.m};
+`;
 
 const PageTemplate = ({ children, pageType }) => (
-  <>
+  <StyledWrapper>
     <Sidebar activeColor={pageType} />
-    {children}
-  </>
+    <SearchWrapper>
+      <Input search placeholder="search..." />
+      <StyledHeading>Notes</StyledHeading>
+      <StyledParagraph>6 notes</StyledParagraph>
+    </SearchWrapper>
+    <StyledGridWrapper>{children}</StyledGridWrapper>
+  </StyledWrapper>
 );
 
 PageTemplate.propTypes = {
--- src/views/Notes/Notes.js
+++ src/views/Notes/Notes.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import PageTemplate from 'templates/PageTemplate/PageTemplate';
+import Card from 'components/molecules/Card/Card';
 
 const Notes = () => (
   <PageTemplate pageType="note">
-    <p>Notes views</p>
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
   </PageTemplate>
 );
 
