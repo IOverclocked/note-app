859f61214c4a1fb8d14a5630184ac97abcc8b91b
IOverclocked <osiatycki.m@gmail.com>
2019-06-22 12:40:29 +0200

    Add configure line for mongoDB

--- _backend/src/controllers/UserController.js
+++ _backend/src/controllers/UserController.js
@@ -7,14 +7,19 @@ const User = mongoose.model('users');
 const user = {
   userLogin: (req, res, next) => {
     passport.authenticate('local', function(err, user, info) {
-      if (err) { return next(err); }
-      if (!user) { return res.sendStatus(403); }
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return res.sendStatus(403);
+      }
 
       req.logIn(user, function(err) {
-        if (err) { return next(err); }
+        if (err) {
+          return next(err);
+        }
         return res.send(user);
       });
-
     })(req, res, next);
   },
   userLogout: (req, res) => {
@@ -23,15 +28,18 @@ const user = {
   },
   userRegister: (req, res) => {
     console.log(req.body.username, req.body.password);
-    User.register(new User({username: req.body.username}), req.body.password, function(err, user) {
+    User.register(new User({ username: req.body.username }), req.body.password, function(
+      err,
+      user,
+    ) {
       if (err) {
         console.log(err);
         return res.sendStatus(500);
-      } else {
+      } 
         passport.authenticate('local')(req, res, function() {
           res.sendStatus(201);
         });
-      }
+      
     });
   },
 };
