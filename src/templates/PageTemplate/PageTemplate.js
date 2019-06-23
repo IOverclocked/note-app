6f03aaa9ef800b180e586e61a3f553e4c4a5e263
IOverclocked <osiatycki.m@gmail.com>
2019-06-23 14:52:21 +0200

    Add forwarding to login page
    if !userId go to login page

--- src/templates/PageTemplate/PageTemplate.js
+++ src/templates/PageTemplate/PageTemplate.js
@@ -2,20 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Sidebar from 'components/organisms/Sidebar/Sidebar';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { routes } from 'routes';
 
 const StyledWrapper = styled.div`
   margin: 0 0 0 150px;
 `;
 
-const PageTemplate = ({ children }) => (
-  <StyledWrapper>
-    <Sidebar />
-    {children}
-  </StyledWrapper>
-);
+const PageTemplate = ({ children, userId }) => {
+  if (!userId) {
+    return <Redirect to={routes.login} />;
+  }
+  return (
+    <StyledWrapper>
+      <Sidebar />
+      {children}
+    </StyledWrapper>
+  );
+};
 
 PageTemplate.propTypes = {
   children: PropTypes.oneOfType([PropTypes.element, PropTypes.node]).isRequired,
+  userId: PropTypes.string,
 };
 
-export default PageTemplate;
+PageTemplate.defaultProps = {
+  userId: null,
+};
+
+const mapStateToProps = ({ userId }) => ({ userId });
+
+export default connect(mapStateToProps)(PageTemplate);
